41b0adcb588d8e96a2cacd65f1dd2cd9e27d18fe
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-06-27 12:05:39 +0100

    finised test suite for generate display, coded initial generation

--- src/generateDisplay.test.ts
+++ src/generateDisplay.test.ts
@@ -1,9 +1,30 @@
 import { generateDisplay } from "./generateDisplay";
 
 test("test when no guessed letters", () => {
-    const guessedLetters = []
+    const guessedLetters: string[] = []
     const answer = 'mandarin'
     const expected = ["_", "_", "_", "_", "_", "_", "_", "_"]
+    expect(generateDisplay(guessedLetters, answer)).toBe(expected);
+});
+
+test("test when no guessed letters correct", () => {
+    const guessedLetters = ['b', 'c', 'e']
+    const answer = 'mandarin'
+    const expected = ["_", "_", "_", "_", "_", "_", "_", "_"]
+    expect(generateDisplay(guessedLetters, answer)).toBe(expected);
+});
+
+test("test display space without guessing", () => {
+    const guessedLetters = ['b', 'c', 'e']
+    const answer = 'a space'
+    const expected = ["_", " ", "_", "_", "_", "_", "e"]
+    expect(generateDisplay(guessedLetters, answer)).toBe(expected);
+});
+
+test("test when double letter guessed", () => {
+    const guessedLetters = ['p']
+    const answer = 'apple'
+    const expected = ["_", "p", "p", "_", "_"]
     expect(generateDisplay(guessedLetters, answer)).toBe(expected);
 });
 
@@ -13,6 +34,3 @@ test("test Neill's example case", () => {
     const expected = ["m", "a", "_", "d", "a", "_", "_", "_"]
     expect(generateDisplay(guessedLetters, answer)).toBe(expected);
 });
-
-//should return: 
-//
--- src/generateDisplay.ts
+++ src/generateDisplay.ts
@@ -2,6 +2,10 @@ function generateDisplay(
     guessedLetters:string[], 
     wordToGuess:string
 ): string[] {
+    const wordWithCorrectGuesses = wordToGuess.split('').map(() => '_')
+
+
+guessedLetters.push(wordToGuess)
 return guessedLetters
 }
 
